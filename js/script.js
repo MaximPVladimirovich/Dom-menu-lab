5f0559fb4967be672170d1c51b7365a70611f4f3
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-05-15 23:36:40 -0500

    Second commit. Task 5.6

--- js/script.js
+++ js/script.js
@@ -16,81 +16,88 @@ let topMenuBg = `var(--top-menu-bg)`;
 topMenuEl.style.setProperty(`background-color`, topMenuBg);
 topMenuEl.classList.add(`flex-around`);
 
+
 //Task 3
-var menuLinks = [
+let menuLinks = [
   {
-    text: "about",
-    href: "/about",
+    text: 'about',
+    href: '/about'
   },
   {
-    text: "catalog",
-    href: "#",
+    text: 'catalog',
+    href: '#',
     subLinks: [
-      {
-        text: "all",
-        href: "/catalog/all",
-      },
-      {
-        text: "top selling",
-        href: "/catalog/top",
-      },
-      {
-        text: "search",
-        href: "/catalog/search",
-      },
-    ],
+      { text: 'all', href: '/catalog/all' },
+      { text: 'top selling', href: '/catalog/top' },
+      { text: 'search', href: '/catalog/search' },
+    ]
   },
   {
-    text: "orders",
-    href: "#",
+    text: 'orders', 
+    href: '#',
     subLinks: [
-      {
-        text: "new",
-        href: "/orders/new",
-      },
-      {
-        text: "pending",
-        href: "/orders/pending",
-      },
-      {
-        text: "history",
-        href: "/orders/history",
-      },
-    ],
+      { text: 'new', href: '/orders/new' },
+      { text: 'pending', href: '/orders/pending' },
+      { text: 'history', href: '/orders/history' },
+    ]
   },
   {
-    text: "account",
-    href: "#",
+    text: 'account', 
+    href: '#',
     subLinks: [
-      {
-        text: "profile",
-        href: "/account/profile",
-      },
-      {
-        text: "sign out",
-        href: "/account/signout",
-      },
-    ],
+      { text: 'profile', href: '/account/profile' },
+      { text: 'sign out', href: '/account/signout' },
+    ]
   },
 ];
-for (let link of menuLinks) {
-  let anchor = document.createElement("a");
-  anchor.textContent = link.text;
-  anchor.setAttribute(`href`, link.href);
 
-  topMenuEl.appendChild(anchor);
-  console.log(anchor);
+for (let link of menuLinks) {
+  let a = document.createElement("a");
+  a.textContent = link.text;
+  a.setAttribute(`href`, link.href);
+  topMenuEl.appendChild(a);
 }
 
+
 //Task 4
-let subMenuEl = document.querySelector(`#sub-menu`);
+let subMenuEl = document.getElementById(`sub-menu`);
 subMenuEl.style.height = `100%`;
-let subMenuBg = `var(--sub-menu-bg)`;
-subMenuEl.style.setProperty(`background-color`, subMenuBg);
+let subMenuBgColor = `var(--sub-menu-bg)`;
+subMenuEl.style.setProperty(`background-color`, subMenuBgColor);
 subMenuEl.classList.add("flex-around");
 subMenuEl.style.setProperty(`position`, `absolute`);
 subMenuEl.style.setProperty(`top`, `0`);
 
+//Task 5
+//Array of a links
+let topMenuLinks = document.querySelectorAll(`a`);
+let showingSubMenu = false;
+
 //Event Listeners
+topMenuEl.addEventListener(`click`, handleClick)
 
 // Functions
+function handleClick(e) {
+  e.preventDefault();
+  if (e.target.matches(`a`)) {
+    e.target.classList.remove(`active`)
+    showingSubMenu = false;
+    subMenuEl.style.setProperty(`top`, `0`);
+    for (let i = 0; i < topMenuLinks.length; i++) {
+      topMenuLinks[i].classList.remove(`active`)
+    }
+    e.target.classList.add(`active`);
+    
+    }
+    return;
+  }
+
+
+
+  // function
+
+
+
+
+
+
